dab78c12a0fa1c25f4e62e613fc48766b2b2d295
VM Sander Mooren <s.c.a.mooren@utwente.nl>
2024-11-05 11:55:17 +0100

    Result dataset working

--- post_processing/result_dataset_original.py
+++ post_processing/result_dataset_original.py
@@ -18,7 +18,6 @@ attribute = "transport.volume_to_capacity_ratio"
 # attribute = "transport.lighting"
 
 
-
 BASE_DIR = Path(__file__).parent
 INIT_DATA_DIR = BASE_DIR / "data/init_data/"
 UPDATES_DIR = BASE_DIR / "data/scenarios/"
@@ -37,7 +36,9 @@ ATTRIBUTES = [
     AttributeSpec("transport.cargo_demand_vkm", DataType(float, csr=True)),
     AttributeSpec("transport.cargo_demand_vkm.peak_yearly", DataType(float, csr=True)),
     AttributeSpec("transport.passenger_demand_vkm", DataType(float, csr=True)),
-    AttributeSpec("transport.passenger_demand_vkm.peak_yearly", DataType(float, csr=True)),
+    AttributeSpec(
+        "transport.passenger_demand_vkm.peak_yearly", DataType(float, csr=True)
+    ),
     AttributeSpec("transport.shortest_path_length", DataType(float, csr=True)),
     AttributeSpec("transport.shortest_path_lane_length", DataType(float, csr=True)),
 ]
@@ -53,27 +54,31 @@ if __name__ == "__main__":
     for scenario in scenarios:
         SCENARIO_UPDATES_DIR = UPDATES_DIR / scenario
 
-        results = SimulationResults(INIT_DATA_DIR, SCENARIO_UPDATES_DIR, timeline_info=timeline_info, attributes=schema)
-        
+        results = SimulationResults(
+            INIT_DATA_DIR,
+            SCENARIO_UPDATES_DIR,
+            timeline_info=timeline_info,
+            attributes=schema,
+        )
+
         dataset = results.get_dataset("bridges")
-        
+
         slice = dataset.slice("bridge_entities", attribute=attribute)
-        dates = [timeline_info.timestamp_to_datetime(t) for t in slice['timestamps']]
-        
+        dates = [timeline_info.timestamp_to_datetime(t) for t in slice["timestamps"]]
+
         dates_jan1_check = []
         for i in np.arange(2019, 2051):
-            index = dates.index(dt.datetime(year = i, month=1, day=1))
+            index = dates.index(dt.datetime(year=i, month=1, day=1))
             dates_jan1_check.append(index)
         dates = [dates[x] for x in dates_jan1_check]
         dates = [dt.datetime.strftime(x, "%Y") for x in dates]
         slice["timestamps"] = dates
         slice["data"] = [slice["data"][x] for x in dates_jan1_check]
 
-
         for i in range(32):
             slice["data"][i] = slice["data"][i]["data"]
 
-        bridges = dict(zip(slice["timestamps"], slice["data"])) 
+        bridges = dict(zip(slice["timestamps"], slice["data"]))
         ic_bridges_df = pd.DataFrame.from_dict(bridges)
         ic_bridges_df.to_csv(f"bridges_lighting_{scenario}.csv", index=True)
 
@@ -87,7 +92,7 @@ if __name__ == "__main__":
         #     dataset.slice(dataset, entity_selector=12),
         #     sep="\n",
         # )
-        # 
+        #
         # print(
         #     "Slicing a dataset over a specific timestamp",
         #     dataset.slice(dataset, timestamp="2020"),
