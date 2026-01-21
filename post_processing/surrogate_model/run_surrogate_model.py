9169fe0d02cfd735c9abb752668ba4d797a06c1a
VM Sander Mooren <s.c.a.mooren@utwente.nl>
2026-01-21 16:09:37 +0100

    Results surrogate model

--- post_processing/surrogate_model/run_surrogate_model.py
+++ post_processing/surrogate_model/run_surrogate_model.py
@@ -10,13 +10,9 @@ from train_surrogate_model import DEFAULT_MODEL_OUT
 
 CUR_DIR = Path(__file__).parent
 CSV_PATH = CUR_DIR / "surrogate_model_inputs_paper_2.csv"
-<<<<<<< HEAD
 PIPELINE_PATH = CUR_DIR / "surrogate_pipeline_gradient_boosting.joblib"
 VARIABLE = "combined_vkt_year_2050"
 
-=======
-PIPELINE_PATH = CUR_DIR / "pipelines" / "surrogate_pipeline_combined_vkt_year_2050_catboost.joblib"
->>>>>>> d8eafbccf672054f9b969f14b442b69a35a3f968
 
 def load_data(path: Path) -> pd.DataFrame:
     """Load data from a CSV file into a DataFrame."""
